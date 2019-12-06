678688dbd654b9f5572c70c257a56e2f9ce6d4d5
RK00333040 <RK00333040@TMIC-LPT013815.TMHaier.ad>
2019-12-06 17:58:00 +0530

    update for duplicate Map key

--- JavaPractice/src/com/raj/collectionsandutils/hashmap/MyDuplicateKeyEx.java
+++ JavaPractice/src/com/raj/collectionsandutils/hashmap/MyDuplicateKeyEx.java
@@ -1,15 +1,37 @@
 package com.raj.collectionsandutils.hashmap;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class MyDuplicateKeyEx {
 
 	public static void main(String[] args) {
-		HashMap<Price,String> hm = new HashMap<Price,String>();
+		HashMap<Price, String> hm = new HashMap<Price, String>();
 		hm.put(new Price("Banana", 20), "Banana");
-	}
-}
+		hm.put(new Price("Apple", 40), "Apple");
+		hm.put(new Price("Orange", 30), "Orange");
+		printMap(hm);
+		
+		Price key = new Price("Banana", 20);
+		System.out.println("Adding duplicate key...");
+		hm.put(key, "Grape");
+		System.out.println("After adding duplicate key:");
+		printMap(hm);
 
+	}
+
+	public static void printMap(HashMap<Price, String> map) {
+		Set<Price> keys = map.keySet();
+        for(Price p:keys) {
+        	System.out.println(p+" ==> "+map.get(p));
+          }
+        }
+	}
+
+/**
+ * @author RK00333040
+ *
+ */
 class Price{
 	private String item;
 	private int price;
@@ -46,6 +68,13 @@ class Price{
 		if (price != other.price)
 			return false;
 		return true;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Price [item=" + item + ", price=" + price + "]";
 	}
 
 	public String getItem() {
