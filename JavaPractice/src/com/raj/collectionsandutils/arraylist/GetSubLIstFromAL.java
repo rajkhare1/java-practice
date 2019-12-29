75251ffabb5098da219849c6d2cefdbd53742ba2
rajkhare <rajkhare@192.168.31.81>
2019-12-29 16:00:01 +0530

    Update ArrayList for sublist

---
+++ JavaPractice/src/com/raj/collectionsandutils/arraylist/GetSubLIstFromAL.java
@@ -0,0 +1,23 @@
+package com.raj.collectionsandutils.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetSubLIstFromAL {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		// adding elements to the end
+		al.add("First");
+		al.add("Second");
+		al.add("Third");
+		al.add("Random");
+		System.out.println("Actual ArrayList: "+al);
+		System.out.println("Now get sublist from this ArrayList by passing....");
+		System.out.println("start index and end index for sublist...");
+		
+		//fromIndex: inclusive ; toIndex: exclusive
+		List<String> subList = al.subList(0, 2);
+		System.out.println("subList: "+subList);
+	}
+}
